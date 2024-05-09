54e6622999a185d2f956a8eaad99dbaaaeae527d
Pietagorh <pietagorhh@proton.me>
2024-05-09 19:01:41 +0200

    Initial commit

---
+++ src/main/java/com/moutonperspicace/betterdndbeyond/BetterDnDBeyondApplication.java
@@ -0,0 +1,13 @@
+package com.moutonperspicace.betterdndbeyond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterDnDBeyondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterDnDBeyondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moutonperspicace/betterdndbeyond/BetterDnDBeyondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moutonperspicace.betterdndbeyond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterDnDBeyondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
